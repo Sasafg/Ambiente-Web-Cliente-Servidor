1557ccad71fc2291936c692394f7077fe2cbda71
ANDREY SOLIS <asolis10963@ufide.ac.cr>
2023-04-16 15:45:35 -0600

    Inclusion de guardado en cookies dependiendo si el usuario es admin o on

--- conexionDB/validaLogin.php
+++ conexionDB/validaLogin.php
@@ -2,24 +2,33 @@
 
 require_once "conexion.php";
 
-//Ingresa la informacion de registro del nuevo usuario a base de datos
+echo "<script type='text/javascript' src='..\cookie\Cookie.js'></script>";
+
 function validaUsuario($ing_usuario, $ing_contrasena)
 {
     $retorno = false;
     $conexion = Conecta();
-    
-    // formato de datos utf8
     if (mysqli_set_charset($conexion, "utf8")){
-        $stmt = $conexion->prepare("select id from usuarios where username = ? and contrasena = ?");
+        $stmt = $conexion->prepare("select id, tipo from usuarios where username = ? and contrasena = ?");
         $stmt->bind_param("ss", $usuario, $contrasena);
 
-        //set parametros y ejecutar
         $usuario = $ing_usuario;
         $contrasena = $ing_contrasena;
 
         if($stmt->execute()){
             $stmt->store_result();
             if ($stmt->num_rows == 1) {
+                $stmt->bind_result($id, $tipo);
+                $stmt->fetch();
+                
+                if ($tipo == 1) {
+                    // Guardar cookie con valor 1 para administrador
+                    echo "<script>setCookie('tipo_usuario', '1', 1);</script>";
+                } else {
+                    // Guardar cookie con valor 2 para usuario común
+                    echo "<script>setCookie('tipo_usuario', '2', 1);</script>";
+                }
+
                 $retorno = true;
             }
         }
