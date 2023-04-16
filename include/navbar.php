44c0d797208d54775ad984688c3637287b7c5bfe
ANDREY SOLIS <asolis10963@ufide.ac.cr>
2023-04-16 16:08:38 -0600

    Inclusion de las opciones administrador al nav bar que solo se muestran a un usuario administrador

--- include/navbar.php
+++ include/navbar.php
@@ -14,9 +14,21 @@
         <li><a href="vehiculos.php">Vehículos</a></li>
         <li><a href="residencia.php">Residencia</a></li>
         <li><a href="contacto.php">Contacto</a></li>
-        <li><a href="registro.php">Registro</a></li>
         <li><a href="resenas.php">Reseñas</a></li>
+        <li><a href="registro.php">Registro</a></li>
         <li><a href="login.php">LogIn</a></li>
+        <li><a href="admin.php" id="adminLink">Opciones Administrador</a></li>
     </ul>
 
 </div>
+
+<script type='text/javascript' src='..\cookie\Cookie.js'></script>
+<script>
+  var tipoUsuario = getCookie("tipo_usuario");
+  if (tipoUsuario !== "1") {
+    var adminLink = document.getElementById("adminLink");
+    if (adminLink) {
+      adminLink.style.display = "none";
+    }
+  }
+</script>
